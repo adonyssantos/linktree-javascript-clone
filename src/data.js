0be3fc021b1013bc72e05e634681a91994098452
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-07-17 13:26:29 -0400

    Update data

--- src/data.js
+++ src/data.js
@@ -1,3 +1,29 @@
-export default {
-  message: 'JavaScript is running!',
+const data = {
+  header: {
+    image: {
+      path: './images/me.jpg',
+      alt: 'Adonys Santos photo',
+    },
+    fullname: 'Adonys Santos',
+    title: 'Junior Frontend Developer',
+  },
+  links: [
+    {
+      title: 'Portfolio',
+      link: 'https://adonyssantos.me/portfolio',
+      openInNewTab: true,
+    },
+    {
+      title: 'GitHub',
+      link: 'https://github.com/adonyssantos',
+      openInNewTab: true,
+    },
+    {
+      title: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/adonyssantos',
+      openInNewTab: true,
+    },
+  ],
 };
+
+export default data;
